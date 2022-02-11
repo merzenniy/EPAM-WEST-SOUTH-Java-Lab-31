c8e3094be825efd1b8fb6acd89ef5290dca6ad9f
Oleksii <alekseygav2001@gmail.com>
2022-02-11 05:30:51 +0200

    Added a validator class

--- src/main/java/com/epam/spring/homework2/beans/ValidateFieldsAnnotationBeanPostProcessor.java
+++ src/main/java/com/epam/spring/homework2/beans/ValidateFieldsAnnotationBeanPostProcessor.java
@@ -1,5 +1,6 @@
 package com.epam.spring.homework2.beans;
 
+import com.epam.spring.homework2.validator.impl.CustomValidatorImpl;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 
@@ -15,6 +16,7 @@ public class ValidateFieldsAnnotationBeanPostProcessor implements BeanPostProces
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
         if (bean.getClass().isAnnotationPresent(ValidateFields.class)) {
             try {
+                CustomValidatorImpl customValidator = new CustomValidatorImpl();
                 Field nameField = bean.getClass().getDeclaredField("name");
                 Field valueField = bean.getClass().getDeclaredField("value");
 
@@ -23,16 +25,8 @@ public class ValidateFieldsAnnotationBeanPostProcessor implements BeanPostProces
 
                 String name = (String) nameField.get(bean);
                 Integer value = (Integer) valueField.get(bean);
-
-                if (name == null) {
-                    System.out.println(bean.getClass().getSimpleName() + " is not valid. " + " Name can not be null");
-
-                } else if (value <= 0) {
-                    System.out.println(bean.getClass().getSimpleName() + " is not valid. " + " Value must be greater than 0");
-
-                } else {
-                    System.out.println(bean.getClass().getSimpleName() + " is valid!)");
-                }
+                customValidator.isNamePresent(name);
+                customValidator.isValuePositive(value);
 
             } catch (NoSuchFieldException | IllegalAccessException e) {
                 e.printStackTrace();
