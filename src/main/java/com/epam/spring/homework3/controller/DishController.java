c84c7fcad7a9432dadafd93bbd3fafbe60aeab8d
Oleksii <alekseygav2001@gmail.com>
2022-02-21 06:56:13 +0200

    Services + controllers

---
+++ src/main/java/com/epam/spring/homework3/controller/DishController.java
@@ -0,0 +1,56 @@
+package com.epam.spring.homework3.controller;
+
+import com.epam.spring.homework3.dto.DishDTO;
+import com.epam.spring.homework3.service.DishService;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class DishController {
+    private final DishService dishService;
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping(value = "/dish/{id}")
+    public DishDTO getDish(@PathVariable int id){
+        log.info(this.getClass().getSimpleName() + ". Inside getDish method");
+        return dishService.getDish(id);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping(value = "/dish")
+    public List<DishDTO> getAllDishes(){
+        log.info(this.getClass().getSimpleName() + ". Inside getAllDishes method");
+        return dishService.dishesList();
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(value = "/dish")
+    public DishDTO createDish(@RequestBody DishDTO dishDTO){
+        log.info(this.getClass().getSimpleName() + ". Inside createDish method");
+        return dishService.createDish(dishDTO);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @PutMapping(value = "/dish/{id}")
+    public DishDTO updateDish(@PathVariable int id, @RequestBody DishDTO dishDTO){
+        log.info(this.getClass().getSimpleName() + ". Inside updateDish method");
+        return dishService.updateDish(id, dishDTO);
+    }
+
+    @DeleteMapping(value = "/dish/{id}")
+    public ResponseEntity<Void> deleteDish(@PathVariable int id){
+        log.info(this.getClass().getSimpleName() + ". Inside deleteDish method");
+        dishService.deleteDish(id);
+        return ResponseEntity.noContent().build();
+    }
+}
